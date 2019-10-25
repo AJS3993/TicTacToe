04e8be2d1ed78e7f8f35b41f07ab9fb9d89021a9
Andrew Smith <ajs3993@gmail.com>
2019-10-25 12:06:06 -0500

    border boxes

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,28 @@
-console.log('hello')
+
+/*----- constants -----*/
+
+
+
+/*----- app's state (variables) -----*/
+
+let TurnNum = 0;
+
+/*----- cached element references -----*/
+
+const A1Box = document.getElementById(A1)
+const A2Box = document.getElementById(A1)
+const A3Box = document.getElementById(A1)
+
+const B1Box = document.getElementById(A1)
+const B2Box = document.getElementById(A1)
+const B3Box = document.getElementById(A1)
+
+const C1Box = document.getElementById(A1)
+const C2Box = document.getElementById(A1)
+const C3Box = document.getElementById(A1)
+
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
