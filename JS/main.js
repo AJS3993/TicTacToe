d8233927021580c1ca431f6984c5f8bad275b8a4
Andrew Smith <ajs3993@gmail.com>
2019-10-25 12:23:33 -0500

    DOM variables

--- JS/main.js
+++ JS/main.js
@@ -10,16 +10,16 @@ let TurnNum = 0;
 /*----- cached element references -----*/
 
 const A1Box = document.getElementById(A1)
-const A2Box = document.getElementById(A1)
-const A3Box = document.getElementById(A1)
+const A2Box = document.getElementById(A2)
+const A3Box = document.getElementById(A3)
 
-const B1Box = document.getElementById(A1)
-const B2Box = document.getElementById(A1)
-const B3Box = document.getElementById(A1)
+const B1Box = document.getElementById(B1)
+const B2Box = document.getElementById(B2)
+const B3Box = document.getElementById(B3)
 
-const C1Box = document.getElementById(A1)
-const C2Box = document.getElementById(A1)
-const C3Box = document.getElementById(A1)
+const C1Box = document.getElementById(C1)
+const C2Box = document.getElementById(C2)
+const C3Box = document.getElementById(C3)
 
 /*----- event listeners -----*/
 
