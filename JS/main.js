251a6808840d423c0099e6b9652ed95053066312
Andrew Smith <ajs3993@gmail.com>
2019-10-27 09:12:51 -0500

    win logic

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,14 @@
 
 /*----- constants -----*/
 
+let win1 = [A1, A2, A3];
+let win2 = [B1, B2, B3]; 
+let win3 = [C1, C2, C3];
+let win4 = [A1, B1, C1];
+let win5 = [A2, B2, C2];
+let win6 = [A3, B3, C3];
+let win7 = [A1, B2, C3];
+let win8 = [A3, B2, C1];
 
 
 /*----- app's state (variables) -----*/
@@ -10,16 +18,8 @@ let turnNum = 0;
 let xArray = [];
 let oArray = [];
 
-winArrays = [
-    [A1, A2, A3], 
-    [B1, B2, B3], 
-    [C1, C2, C3],
-    [A1, B1, C1],
-    [A2, B2, C2],
-    [A3, B3, C3],
-    [A1, B2, C3],
-    [A3, B2, C1]
-];
+
+
 
 
 
@@ -37,6 +37,7 @@ const C1Box = document.getElementById('C1');
 const C2Box = document.getElementById('C2');
 const C3Box = document.getElementById('C3');
 
+
 /*----- event listeners -----*/
 
 A1Box.addEventListener("click", play);
@@ -52,26 +53,165 @@ C2Box.addEventListener("click", play);
 C3Box.addEventListener("click", play);
 
 
+
 /*----- functions -----*/
 
 function play(){
-    if (this.className == "played"){
+    if (this.className == "oplayed"){
+        return
+    }
+    if (this.className == "xplayed"){
         return
     }
     turnNum += 1;
     if (turnNum%2 == 0){
         this.textContent = "O"
         oArray.push(this.id)
+        this.className = "oplayed"
     }
     if (turnNum%2 == 1){
         this.textContent = "X"
         xArray.push(this.id)
+        this.className = "xplayed"
     }
-        this.className = "played"
-    return;
-};
+        render()
+        return;
+    }
+    
 
 
-if (winArrays.includes(xArray) == true){
-    alert ("hi")
+function render(){
+    if (xArray.includes('A1')){
+        if (xArray.includes('A2')){
+            if (xArray.includes('A3')){ 
+                alert('x wins') 
+            }  
+        }
+    }
+    
+    if (xArray.includes('B1')){
+        if (xArray.includes('B2')){
+            if (xArray.includes('B3')){ 
+                alert('x wins') 
+            }  
+        }
+    }
+
+    if (xArray.includes('C1')){
+        if (xArray.includes('C2')){
+            if (xArray.includes('C3')){ 
+                alert('x wins') 
+            }  
+        }
+    }
+
+    if (xArray.includes('A1')){
+        if (xArray.includes('B1')){
+            if (xArray.includes('C1')){ 
+                alert('x wins') 
+            }  
+        }
+    }
+
+    if (xArray.includes('A2')){
+        if (xArray.includes('B2')){
+            if (xArray.includes('C2')){ 
+                alert('x wins') 
+            }  
+        }
+    }
+
+    if (xArray.includes('A3')){
+        if (xArray.includes('B3')){
+            if (xArray.includes('C3')){ 
+                alert('x wins') 
+            }  
+        }
+    }
+
+    if (xArray.includes('A1')){
+        if (xArray.includes('B2')){
+            if (xArray.includes('C3')){ 
+                alert('x wins') 
+            }  
+        }
+    }
+
+    if (xArray.includes('A3')){
+        if (xArray.includes('B2')){
+            if (xArray.includes('C1')){ 
+                alert('x wins') 
+            }  
+        }
+    }
+
+///////////////
+
+
+if (oArray.includes('A1')){
+    if (oArray.includes('A2')){
+        if (oArray.includes('A3')){ 
+            alert('o wins') 
+        }  
+    }
 }
+
+if (oArray.includes('B1')){
+    if (oArray.includes('B2')){
+        if (oArray.includes('B3')){ 
+            alert('o wins') 
+        }  
+    }
+}
+
+if (oArray.includes('C1')){
+    if (oArray.includes('C2')){
+        if (oArray.includes('C3')){ 
+            alert('o wins') 
+        }  
+    }
+}
+
+if (oArray.includes('A1')){
+    if (oArray.includes('B1')){
+        if (oArray.includes('C1')){ 
+            alert('o wins') 
+        }  
+    }
+}
+
+if (oArray.includes('A2')){
+    if (oArray.includes('B2')){
+        if (oArray.includes('C2')){ 
+            alert('o wins') 
+        }  
+    }
+}
+
+if (oArray.includes('A3')){
+    if (oArray.includes('B3')){
+        if (oArray.includes('C3')){ 
+            alert('o wins') 
+        }  
+    }
+}
+
+if (oArray.includes('A1')){
+    if (oArray.includes('B2')){
+        if (oArray.includes('C3')){ 
+            alert('o wins') 
+        }  
+    }
+}
+
+if (oArray.includes('A3')){
+    if (oArray.includes('B2')){
+        if (oArray.includes('C1')){ 
+            alert('o wins') 
+        }  
+    }
+}
+
+
+}
+
